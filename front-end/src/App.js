20e56343b75e8445128cb2e866d3f13471ad4f72
Zhaoping Yu <yuzhaoping1970@gmail.com>
2021-09-19 08:51:43 -0400

    Setup Google Material Icons

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,9 +3,13 @@ import React from 'react';
 const App = () => {
 
   return (
+    <React.Fragment>
     <div className='hello font-dosis' >
       {'Hello Veritone'} <span className='font-nunito'>to Challenge</span>
     </div>
+    <div className='material-icons-outlined'>mode_edit</div>
+    <div className='material-icons-outlined'>delete</div>
+    </React.Fragment>
   );
 };
 
