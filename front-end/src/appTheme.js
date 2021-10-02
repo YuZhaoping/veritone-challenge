03f9adf80e26e39dacc989bc9aa0d907b2796659
Zhaoping Yu <yuzhaoping1970@gmail.com>
2021-10-02 19:16:07 -0400

    Change to the Roboto font

--- front-end/src/appTheme.js
+++ front-end/src/appTheme.js
@@ -2,11 +2,6 @@ import createTheme from '@material-ui/core/styles/createTheme';
 
 const theme = createTheme({
   typography: {
-    fontFamily: [
-      'Nunito',
-      'sans-serif',
-    ].join(','),
-    fontWeightBold: 600,
     button: {
       textTransform: 'none',
     },
